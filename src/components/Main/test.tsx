a748dc56a790a62f40af884155a705c3745a443b
thigrush <thigrush@gmail.com>
2021-02-04 19:39:39 -0300

    feat: add RTL config and first test

---
+++ src/components/Main/test.tsx
@@ -0,0 +1,14 @@
+import { render, screen } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
+
+import Main from '.'
+
+describe('<Main />', () => {
+  it('should render the heading', () => {
+    render(<Main />)
+
+    expect(
+      screen.getByRole('heading', { name: /react avançado/i })
+    ).toBeInTheDocument()
+  })
+})
